cf0a7112e6b9582693deeba5b138ccb0e36ab48d
Magdalena Szumna <szumna.m@gmail.com>
2015-04-27 21:55:21 +0200

    add dummy test for module merge

---
+++ src/app/merge/merge.spec.js
@@ -0,0 +1,7 @@
+describe('merge section', function () {
+    beforeEach(module('manual-merge.merge'));
+
+    it('should have a dummy test', inject(function() {
+        expect(true).toBeTruthy();
+    }));
+});
