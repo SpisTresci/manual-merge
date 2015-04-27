60a6b5417f3e0b7d61bb9475ec00b9886943382a
Magdalena Szumna <szumna.m@gmail.com>
2015-04-27 20:57:55 +0200

    add new subpages merge

---
+++ src/app/merge/merge.js
@@ -0,0 +1,15 @@
+(function(app) {
+
+    app.controller('MergeController', function ($scope) {
+        var model = this;
+
+        init();
+
+        function init() {
+            // A definitive place to put everything that needs to run when the controller starts. Avoid
+            //  writing any code outside of this function that executes immediately.
+        }
+
+    });
+
+}(angular.module("manual-merge.merge")));
